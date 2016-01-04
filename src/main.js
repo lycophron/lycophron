5220b38d85a6546fc0c03c2644f2661023237a74
Zsolt Lattmann <zsolt.lattmann@gmail.com>
2016-01-04 13:41:03 +0100

    Fixed blank look up issue with dictionary.

--- src/main.js
+++ src/main.js
@@ -43,43 +43,30 @@ window.runTest2 = function () {
 
 window.runTest = function () {
   var Tile = L.Tile;
-  var dictionary = new L.Dictionary('en' + '/' + 'WL2014' + '.json');
+  var dictionary = new L.Dictionary('hu' + '/' + 'me2003' + '.json');
   var board = new L.Board();
   var score = new L.Score(board, dictionary);
   var rack = new L.Rack();
   var solver = new L.Solver(board, score, dictionary);
+
   // setup board
-  board.setTile(14, 0, new Tile('a', 4));
-  board.setTile(14, 1, new Tile('b', 4));
-  board.setTile(14, 2, new Tile('c', 4));
-  board.setTile(14, 3, new Tile('d', 4));
-  board.setTile(14, 4, new Tile('e', 4));
-  board.setTile(14, 5, new Tile('f', 4));
-  board.setTile(14, 6, new Tile('g', 8));
-  board.setTile(14, 8, new Tile('x', 8));
-  board.setTile(14, 9, new Tile('c', 4));
-  board.setTile(14, 10, new Tile('h', 4));
-  board.setTile(14, 11, new Tile('i', 4));
-  board.setTile(14, 12, new Tile('j', 4));
-  board.setTile(14, 13, new Tile('k', 4));
-  board.setTile(14, 14, new Tile('l', 4));
+  board.setTile(4, 7, new Tile('b', 2));
+  board.setTile(5, 7, new Tile('E', 3));
+  board.setTile(6, 7, new Tile('k', 1));
+  board.setTile(7, 7, new Tile('a', 1));
+  board.setTile(4, 8, new Tile('Q', 4));
+  board.setTile(4, 9, new Tile('j', 4));
+  board.setTile(4, 10, new Tile('t', 1));
 
-  board.setTile(13, 7, new Tile('c', 8));
-  board.setTile(0, 6, new Tile('x', 8));
-  board.setTile(0, 8, new Tile('x', 8));
-
-  board.setTile(5, 7, new Tile('z', 10));
-  board.setTile(7, 7, new Tile('p', 4));
   // set rack
-  rack.addTile(new Tile('s', 1));
-  rack.addTile(new Tile('a', 1));
-  rack.addTile(new Tile('e', 1));
-  rack.addTile(new Tile('e', 1));
-  rack.addTile(new Tile('e', 1));
-  rack.addTile(new Tile('?', 0));
+  rack.addTile(new Tile('X', 4));
+  rack.addTile(new Tile('h', 3));
 
-  dictionary.ready()
+  return dictionary.ready()
     .then(function () {
+      console.log('here');
+      solver.getSolution(['?', 'E'], null, ['X', 'h']);
+      
       var solutions = solver.solve(rack);
       console.log(solutions);
       console.error('done');
@@ -120,7 +107,7 @@ window.runTest3 = function () {
   rack.addTile(new Tile('e', 1));
   rack.addTile(new Tile('a', 1));
 
-  dictionary.ready()
+  return dictionary.ready()
     .then(function () {
       var solutions = solver.solve(rack);
       console.log(solutions);
@@ -130,7 +117,7 @@ window.runTest3 = function () {
 };
 
 $('#test').click(function () {
-  runTest2()
+  runTest()
     .then(function (generatedGame) {
       if (generatedGame) {
         loadGame(generatedGame);
