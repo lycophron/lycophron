4e6e639986eab5794cedb9a3a7260d9811189aad
Zsolt Lattmann <zsolt.lattmann@gmail.com>
2016-01-06 15:22:16 +0100

    Add fixture to load dictionary only once for bug report tests.

---
+++ test/bugs/fixture.js
@@ -0,0 +1,8 @@
+
+var L = require('../../lib/lycophron');
+var Dictionary = L.Dictionary;
+
+module.exports = {
+  'hu/me2003.json': new Dictionary('hu/me2003.json'),
+  'en/WL2014.json': new Dictionary('en/WL2014.json')
+}
